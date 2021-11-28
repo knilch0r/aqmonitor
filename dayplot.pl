000001037b137ff34ffeea178f0d05fc69ae9887
knilch0r <knilch0r@users.noreply.github.com>
2021-11-28 14:19:49 +0100

    handle 2-in-1 plots

--- dayplot.pl
+++ dayplot.pl
@@ -11,7 +11,7 @@ use POSIX qw(fmod);
 # for axis: 1px axis line, up to 10px marks
 # and a border of 1px all around
 
-my $im = new GD::Image(24*24+11+2, 300+11+2);
+my $im = new GD::Image(24*24+11+11+2, 300+11+2);
 
 # colors: background white
 my $white = $im->colorAllocate(255, 255, 255);
@@ -20,24 +20,29 @@ my $lgrey = $im->colorAllocate(211, 211, 211);
 my $llgrey = $im->colorAllocate(225, 225, 225);
 # black for axis
 my $black = $im->colorAllocate(0, 0, 0);
-# red for temp
-my $red = $im->colorAllocate(211, 0, 0);
-# blue for hum TODO
-my $blue = $im->colorAllocate(0, 0, 211);
+# red for first axis
+my $red = $im->colorAllocate(190, 0, 0);
+# blue for second axis
+my $blue = $im->colorAllocate(90, 90, 255);
 
 # read in data
-my $maxa;
-my $mina;
+my ($maxa, $mina, $maxb, $minb);
 my @data;
 
 while (<>) {
-	die "invalid data '$_'" unless /^(\d\d) (\d\d) (\d\d) ([0-9.]+)/;
-	my ($h, $m, $s, $v) = ($1, $2, $3, $4);
-	$maxa //= $v;
-	$mina //= $v;
-	$maxa = $v if ($v > $maxa);
-	$mina = $v if ($v < $mina);
-	push @data, [$h, $m, $s, $v];
+	die "invalid data '$_'" unless /^(\d\d) (\d\d) (\d\d) ([0-9.]+)( ([0-9.]+))?/;
+	my ($h, $m, $s, $va, $vb) = ($1, $2, $3, $4, $6);
+	$maxa //= $va;
+	$mina //= $va;
+	$maxa = $va if ($va > $maxa);
+	$mina = $va if ($va < $mina);
+	if (defined $vb) {
+		$maxb //= $vb;
+		$minb //= $vb;
+		$maxb = $vb if ($vb > $maxb);
+		$minb = $vb if ($vb < $minb);
+	}
+	push @data, [$h, $m, $s, $va, $vb];
 }
 
 # draw x axis
@@ -83,39 +88,72 @@ my $rnga = $maxa - $mina;
 my $bstep = int(10**int(log($rnga)/log(10)));
 my $sstep = $bstep / 10;
 
+my $MAXX = getxforhms(24,0,0);
+
 my $sstart = $mina - POSIX::fmod($mina, $sstep) + $sstep;
 for ( my $i = $sstart; $i < $maxa ; $i += $sstep ) {
 	my $y = getyforval($i, $maxa, $mina);
 	$im->line(7, $y, 12, $y, $black);
-	$im->line(13, $y, getxforhms(24,0,0), $y, $llgrey);
+	$im->line(13, $y, $MAXX, $y, $llgrey);
 }
 
 my $bstart = $mina - POSIX::fmod($mina, $bstep) + $bstep;
 for ( my $i = $bstart; $i < $maxa ; $i += $bstep ) {
 	my $y = getyforval($i, $maxa, $mina);
 	$im->line(2, $y, 12, $y, $black);
-	$im->line(13, $y, getxforhms(24,0,0), $y, $lgrey);
+	$im->line(13, $y, $MAXX, $y, $lgrey);
 }
+
+# draw second y axis if there were values
+if (defined $maxb) {
+
+	my $rngb = $maxb - $minb;
+	$bstep = int(10**int(log($rngb)/log(10)));
+	$sstep = $bstep / 10;
+
+	my $sstart = $minb - POSIX::fmod($minb, $sstep) + $sstep;
+	for ( my $i = $sstart; $i < $maxb ; $i += $sstep ) {
+		my $y = getyforval($i, $maxb, $minb);
+		$im->line($MAXX, $y, $MAXX+5, $y, $black);
+	}
+
+	my $bstart = $minb - POSIX::fmod($minb, $bstep) + $bstep;
+	for ( my $i = $bstart; $i < $maxb ; $i += $bstep ) {
+		my $y = getyforval($i, $maxb, $minb);
+		$im->line($MAXX, $y, $MAXX+10, $y, $black);
+	}
+
+} # if second axis
 
 # print data
 
 $im->setThickness(2);
-my ($lt, $lx, $ly);
+my ($lt, $lx, $lya, $lyb);
 for my $i (@data) {
-	my ($h, $m, $s, $v) = @$i;
+	my ($h, $m, $s, $va, $vb) = @$i;
 	my $x = getxforhms($h, $m, $s);
-	my $y = getyforval($v, $maxa, $mina);
+	my $ya = getyforval($va, $maxa, $mina);
+	my $yb;
+	if (defined $maxb) {
+		$yb = getyforval($vb, $maxb, $minb);
+	}
 	my $t = $h*60+$m;
 	$lt //= $t;
 	$lx //= $x;
-	$ly //= $y;
+	$lya //= $ya;
+	$lyb //= $yb;
 	if (abs($t-$lt) > 10) {
 		$lx = $x;
-		$ly = $y;
+		$lya = $ya;
+		$lyb = $yb;
 	}
-	$im->line($lx, $ly, $x, $y, $red);
+	if (defined $maxb) {
+		$im->line($lx, $lyb, $x, $yb, $blue);
+	}
+	$im->line($lx, $lya, $x, $ya, $red);
 	$lx = $x;
-	$ly = $y;
+	$lya = $ya;
+	$lyb = $yb;
 	$lt = $t;
 }
 
